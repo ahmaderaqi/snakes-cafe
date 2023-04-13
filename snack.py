7fed5893fdb75bbc36a6ed9c8a40131b77153d13
ahmaderaqi <ahmadzaher573@gmail.com>
2023-04-13 15:42:52 +0000

    lab1

---
+++ snack.py
@@ -0,0 +1,35 @@
+print("$ python snakes_cafe.py\n**************************************\n**    Welcome to the Snakes Cafe!   **\n**    Please see our menu below.    **")
+print("**\n** To quit at any time, type \"quit\" **\n**************************************\n")
+print("what would you like to order?\n")
+
+order_array = []
+taken_arr = ["a"]
+final_arr=[]
+order = ""
+order = input(">")
+order_array.append(order)
+def coffe():
+    counter = 0
+    
+    for i in range(len(order_array)):
+        
+                for j in range(len(order_array)):
+                    if (order_array[i] == order_array[j]):
+                        counter += 1
+                        taken_arr.append(order_array[i])
+
+                print(f"{counter} of {order_array[i]} has been added to your menu\n")
+
+                counter = 0
+    
+while order != "quit":
+    print("what would you like to order?\n")
+    order = input(">")
+    order_array.append(order)
+    coffe()
+    
+    
+    
+
+
+print(order_array)
